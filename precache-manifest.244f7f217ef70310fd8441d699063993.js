f0cceeb54660add767c7fcecd0aaff238a29149b
Kumar Shyam <Shyam.Kumar@lisec.com>
2020-01-30 08:50:57 +0400

    Updates

---
+++ precache-manifest.244f7f217ef70310fd8441d699063993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c35a30707db286901df23b4f239e4e",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "ca08a0788db188fe75eb",
+    "url": "/myportfolio/static/css/main.cc0b3a03.chunk.css"
+  },
+  {
+    "revision": "76077e23fc67919b048f",
+    "url": "/myportfolio/static/js/2.a7f3bb74.chunk.js"
+  },
+  {
+    "revision": "ca08a0788db188fe75eb",
+    "url": "/myportfolio/static/js/main.bc32b624.chunk.js"
+  },
+  {
+    "revision": "a6dec94f35a78ae943c0",
+    "url": "/myportfolio/static/js/runtime~main.b7ee7db6.js"
+  }
+]);
